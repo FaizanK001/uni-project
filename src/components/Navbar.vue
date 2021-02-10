2c6db037125dd4a3ff5b0e5af1b16dce1a579352
Huberto485 <thehubert.antczak@gmail.com>
2021-02-10 18:43:22 +0000

    Changed names of pages 'About' and 'Services'; + added 'Account' page, added appropriate routes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,26 +2,29 @@
 <div>
 <!-- Navigation -->
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-    <li class="nav-item active">
-      <a class="navbar-brand" to="/">Cardiomyopathy  <i class="fas fa-heartbeat"></i>  </a>
-    </li>
+  <li class="nav-item active">
+    <a class="navbar-brand" to="/">Cardiomyopathy <i class="fas fa-heartbeat"></i></a>
+  </li>
+
   <ul class="navbar-nav  ml-auto">
     <li class="nav-item">
-       <router-link class="nav-link" to="/">Home</router-link>
+      <router-link class="nav-link" to="/">Home</router-link>
     </li>
     <li class="nav-item">
-      <router-link class="nav-link" to="/services">Services</router-link>
+      <router-link class="nav-link" to="/mutation-data">Mutation Data</router-link>
     </li>
     <li class="nav-item">
-     <router-link class="nav-link" to="/about">About</router-link>
+      <router-link class="nav-link" to="/help">Help</router-link>
     </li>
-   <li>
-     <router-link  class="btn btn-success" to="/login">Sign In</router-link> 
-   </li>
     
-  <li>
+      
+
+    <li>
+      <router-link  class="btn btn-success" to="/login">Sign In</router-link> 
+    </li>
+    <li>
       <router-link class="btn btn-success" to="/register">Register</router-link>
-     </li>
+    </li>
   </ul>
 
 </nav>
---
+++ src/views/Account.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="account">
+
+    <h1>this is an account page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Account',
+  components: {
+  
+  }
+}
+</script>

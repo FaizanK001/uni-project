261f7dc4f012a3994a20c9955f3ad3c0ef39f74c
Mahomed Ebrahim <mahomed98@hotmail.com>
2021-02-10 17:57:30 +0000

    Carousel
    slides

---
+++ src/components/Carousel.vue
@@ -0,0 +1,63 @@
+<template>
+ <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="../assets/cardiomyopathy-chart.jpg" alt="Los Angeles">
+    </div>
+
+    <div class="item">
+      <img src="../assets/cardiomyopathy-chart.jpg" alt="Chicago">
+    </div>
+
+    <div class="item">
+      <img src="../assets/logo.png" alt="New York">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+</template>
+
+<script>
+// @ is an alias to /src
+  
+export default {
+  name: 'Home',
+  components: {
+  
+  },
+  data() {
+      return {
+        slide: 0,
+        sliding: null
+      }
+    },
+    methods: {
+      onSlideStart() {
+        this.sliding = true
+      },
+      onSlideEnd() {
+        this.sliding = false
+      }
+    }
+  }
+
+
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,18 @@
   <div class="home">
 
     <h1>this is home page</h1>
+    <Carousel/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-
+import Carousel from '../components/Carousel.vue'
 export default {
   name: 'Home',
   components: {
-  
+  Carousel
   }
 }
 </script>

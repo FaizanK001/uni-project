586042c51276dfb2d03dc50b584aaea0a00c0ec6
FaizanK001 <47453699+FaizanK001@users.noreply.github.com>
2021-02-10 16:52:10 +0000

    Firebase connection
    Made Firebase connection

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 import { createApp } from 'vue'
+//import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from "firebase/app"
+//import axios from "axios";
 
-//import './assets/css/Navbar.css'
+
+
+//Vue.prototype.$axios = axios;
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDhtg4_cHjKbL-EPa8CPaXT4NS9pHpqgMY",
+    authDomain: "cardiomyopathy-p.firebaseapp.com",
+    projectId: "cardiomyopathy-p",
+    storageBucket: "cardiomyopathy-p.appspot.com",
+    messagingSenderId: "740652095333",
+    appId: "1:740652095333:web:6d6c590d44b0a1a96eb38c"
+  };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
 createApp(App).use(router).mount('#app')

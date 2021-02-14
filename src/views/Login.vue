5031901b944e656343ed7a0810ed572b557a1e04
FaizanK001 <47453699+FaizanK001@users.noreply.github.com>
2021-02-14 04:59:48 +0000

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -10,14 +10,14 @@
           <div class="row">
             <div class="col-md-9 col-lg-8 mx-auto">
               <h3 class="login-heading mb-4">Welcome back!</h3>
-              <form>
+              <form @submit.prevent>
                 <div class="form-label-group">
-                  <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+                  <input type="email" v-model="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
                   <label for="inputEmail">Email address</label>
                 </div>
 
                 <div class="form-label-group">
-                  <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                  <input type="password" v-model="password"  id="inputPassword" class="form-control" placeholder="Password" required>
                   <label for="inputPassword">Password</label>
                 </div>
 
@@ -25,7 +25,12 @@
                   <input type="checkbox" class="custom-control-input" id="customCheck1">
                   <label class="custom-control-label" for="customCheck1">Remember password</label>
                 </div>
-                <button class="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2" type="submit">Sign in</button>
+                <el-form-item v-if="error">
+                <el-button plain disabled icon="el-icon-error">{{error}}</el-button>
+                </el-form-item>
+
+                <button class="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2" @click="login">Sign in</button>
+
                 <div class="text-center">
                   <a class="small" href="#">Forgot password?</a></div>
               </form>
@@ -41,6 +46,8 @@
 </template>
 
 <script>
+import {ref} from "vue";
+import {firebase}
 export default {
 data: () =>{
   return{

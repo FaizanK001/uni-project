bb72479f1c2c5cd30b1097ccf1308eadb7d925ac
Akram-shaban <Akram.shaban98@gmail.com>
2021-02-26 15:14:06 +0000

    updates

--- src/firebase/database.js
+++ src/firebase/database.js
@@ -14,6 +14,7 @@ const firebaseConfig = {
   measurementId: "G-BX82H1662V"
 };
 
+
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
     const firebaseAuthentication = firebase.auth();
